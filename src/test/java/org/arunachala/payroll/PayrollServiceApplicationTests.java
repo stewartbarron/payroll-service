6cd1446e1693d5e6ec904a0e295c86f2b0537ad8
stewartbarron <stewart.barron@mac.com>
2024-05-04 11:38:49 +0100

    intial commit

---
+++ src/test/java/org/arunachala/payroll/PayrollServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arunachala.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
